988ba3dae5514354956314fd7d317107d6dd8249
Apostol85 <pav.tasits@yandex.ru>
2021-07-19 21:43:38 +0300

    444

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {Helmet} from 'react-helmet-async';
+import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import Container from '@material-ui/core/Container';
-import {Tile} from '../../components/Tile';
-import {makeStyles} from "@material-ui/core/styles";
+import { Tile } from '../../components/Tile';
+import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
-import { useHomepageSliceSlice} from "./slice";
-import { selectHomepageSlice} from "./slice/selectors";
-import _ from 'lodash'
+import { useHomepageSliceSlice } from './slice';
+import { selectHomepageSlice } from './slice/selectors';
+import _ from 'lodash';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -35,52 +35,56 @@ const getColor = () => {
 };
 
 interface IArrayWithColor {
-  id: number,
-  color: string
+  id: number;
+  color: string;
 }
 
-const compareColors = (arr) => {
-  _.forEach(arr, function(value, key) {
-    console.log('key', key)
-    console.log('value', value)
-    _.filter(value, function (o) {console.log(o.color)})
-  })
-}
+const compareColors = arr => {
+  _.forEach(arr, function (value) {
+    const r = _.filter(arr, function (o) {
+      return o.color === value.color;
+    });
+    console.log('r', r);
+    if (r.length > 1) {
+      return r;
+    }
+    console.log('not break');
+  });
+};
 
 export const HomePage = () => {
   const storeArr = useSelector(selectHomepageSlice);
-  const [arrayWithColor, setArrayWithColor] = useState<IArrayWithColor[]>([])
+  const [arrayWithColor, setArrayWithColor] = useState<IArrayWithColor[]>([]);
+  const [comparedColorsArr, setComparedColorsArr] = useState<IArrayWithColor[]>([]);
 
   const tilesArr = [
-    {id: 1, color: ''},
-    {id: 2, color: ''},
-    {id: 3, color: ''},
-    {id: 4, color: ''},
-    {id: 5, color: ''},
-    {id: 6, color: ''},
-    {id: 7, color: ''},
-    {id: 8, color: ''},
-    {id: 9, color: ''},
-    {id: 10, color: ''},
-    {id: 11, color: ''},
-    {id: 12, color: ''}
-  ]
+    { id: 1, color: '' },
+    { id: 2, color: '' },
+    { id: 3, color: '' },
+    { id: 4, color: '' },
+    { id: 5, color: '' },
+    { id: 6, color: '' },
+    { id: 7, color: '' },
+    { id: 8, color: '' },
+    { id: 9, color: '' },
+    { id: 10, color: '' },
+    { id: 11, color: '' },
+    { id: 12, color: '' },
+  ];
   tilesArr.forEach(item => {
-    item.color = getColor() as string
-  })
+    item.color = getColor() as string;
+  });
   useEffect(() => {
     tilesArr.forEach(item => {
-      item.color = getColor() as string
-    })
-    setArrayWithColor(tilesArr)
-  }, [])
-
-  console.log('storeArr', storeArr)
+      item.color = getColor() as string;
+    });
+    setArrayWithColor(tilesArr);
+  }, []);
 
   useEffect(() => {
-    compareColors(storeArr)
-    //console.log('!!!', r)
-  }, [storeArr])
+    console.log('compareColors(storeArr)', compareColors(storeArr));
+    //setComparedColorsArr(compareColors(storeArr));
+  }, [storeArr]);
 
   const classes = useStyles();
 
@@ -96,16 +100,10 @@ export const HomePage = () => {
       <Container maxWidth="sm">
         <div className={classes.root}>
           {arrayWithColor.map(item => {
-            return (
-              <Tile
-                key={item.id}
-                id={item.id}
-                color={item.color}
-              />
-            )
+            return <Tile key={item.id} id={item.id} color={item.color} />;
           })}
         </div>
       </Container>
     </>
   );
-}
+};
